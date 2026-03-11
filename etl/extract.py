106b1dda3317396b4751f330073449864fc3857f
Jordan Brooks <jordanbrooks4@aol.com>
2026-03-11 16:34:21 -0400

    Initial project structure

---
+++ etl/extract.py
@@ -0,0 +1,4 @@
+def extract_data():
+    print("Extracting data...")
+    # TODO: load dataset
+    return None
---
+++ etl/transform.py
@@ -0,0 +1,4 @@
+def transform_data(data):
+    print("Transforming data...")
+    
+    return data
